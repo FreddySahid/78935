02ad114b51c07eddc3dce287713b353d0b2b53c4
unknown <freddysahid@outlook.com>
2022-03-30 18:06:55 -0600

    Saludos

---
+++ Saludos/src/main/java/com/example/t4is/Saludos/saludos.java
@@ -0,0 +1,29 @@
+package com.example.t4is.Saludos;
+
+public class saludos {
+    int id;
+    String nombre;
+    saludos( ){
+        
+
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+
+    
+}
